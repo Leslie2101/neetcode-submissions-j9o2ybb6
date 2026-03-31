6b869f1df4fb8e2686d4a5b09c7d32e056cb6a90
MilkteaForLife <109104110+Leslie2101@users.noreply.github.com>
2026-04-01 04:15:44 +1030

    Add: maximum-product-subarray - submission-2

---
+++ Data Structures & Algorithms/maximum-product-subarray/submission-2.py
@@ -0,0 +1,34 @@
+import math
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        # keep track of biggest positive, smallest negative 
+        pos = 1
+        neg = None
+
+        res = -math.inf 
+
+        for num in nums:
+
+            res = max(res, num)
+
+            if num > 0:
+                pos = pos * num if pos != None else num
+                neg = neg * num if neg != None else None 
+
+            elif num == 0:
+                pos = neg = None 
+
+            else:
+                newneg = pos * num if pos != None else num 
+                newpos = neg * num if neg != None else None
+                pos, neg = newpos, newneg
+                
+            
+                    
+            res = max(res, num)
+            if pos != None: res = max(res, pos)
+            if neg != None: res = max(res, neg)
+
+        
+        return res
+
